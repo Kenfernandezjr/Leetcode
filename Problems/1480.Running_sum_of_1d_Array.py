8a27057d264dbdf79e2371cb80673fd5d65c7a6e
kenfernandezjr <kenfernandezjr@github.com>
2020-08-31 09:31:17 -0700

    leet coding for the day 8.31

---
+++ Problems/1480.Running_sum_of_1d_Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        Result = []
+        boo = 0
+        for i in (nums):
+            sum(nums, i)
+            boo += i
+            Result.append(boo)
+        return Result
