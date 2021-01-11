e4f2965bede46e7ec769478407d86617655b5dce
kenfernandezjr <kenfernandezjr@github.com>
2021-01-11 10:09:56 -0800

    Modifying string

---
+++ Problems/1108.Defanging_an_IP_Address.py
@@ -0,0 +1,16 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        # completed this 3 different ways
+        # First
+         changeList = list(address)
+         for i in range(len(changeList)):
+             if changeList[i] is ".":
+                 changeList[i] = "[.]"
+         return("".join(changeList))
+        # Second
+        return "".join(["[.]" if i is "." else i for i in list(address)])
+
+        # Third
+        return address.replace(".", "[.]")
+        
+
